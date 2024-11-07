b797f1a3e9ae14aedeccae4046d00c4e65b7e4ab
Ojas Baral <ojas.baral@gmail.com>
2024-11-06 20:35:17 -0700

    updated site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/header'
 import content from './data/content.json'
 import Resume from './data/resume.pdf'
 import Img from './data/IMG_8122.JPG'
+import myface from './data/myface.JPG'
 import { CSSTransition } from 'react-transition-group';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <div className="h-screen max-w-[1560px] grid place-items-center grid-flow-row m-auto">
       <Header></Header>
       <div className="text-center main-page">
-        <h1 className="text-4xl font-bold pb-2">Ojas Baral</h1>
+        <h1 className="text-4xl font-bold pb-2 text-dblue">Ojas Baral</h1>
         <h1 className="text-lg pb-2">Computer Science Student</h1>
         <h1><a className="text-[#3562ff] hover:underline cursor-pointer" href="https://www.linkedin.com/in/ojasbaral" target="_blank">https://www.linkedin.com/in/ojasbaral</a ></h1>
         <h1><a className="text-[#3562ff] hover:underline cursor-pointer" href="https://github.com/ojasbaral" target="_blank">https://github.com/ojasbaral</a ></h1>
@@ -19,7 +20,7 @@ function App() {
     <div id="about" className="h-[75px]"></div>
 
     <div className="max-w-[1200px] m-auto p-4">
-      <h1 className="text-2xl">About</h1>
+      <h1 className="text-2xl text-dblue">About</h1>
       <h1 className="pt-4">{content.about.content}</h1>
 
       <h1 className="pt-8">GPA: {content.about.GPA}</h1>
@@ -30,7 +31,7 @@ function App() {
     <div id="experience" className="h-[75px]"></div>
 
     <div className="max-w-[1200px] m-auto p-4">
-      <h1 className="text-2xl">Experience</h1>
+      <h1 className="text-2xl text-dblue">Experience</h1>
       {content.exp.map((work, index) => (
         <div id={index} className="pt-4">
           <h1 className="text-lg">{work.company}</h1>
@@ -50,7 +51,7 @@ function App() {
     <div id="projects" className="h-[75px]"></div>
 
     <div className="max-w-[1200px] m-auto p-4">
-      <h1 className="text-2xl">Projects</h1>
+      <h1 className="text-2xl text-dblue">Projects</h1>
       {content.projects.map((project, index) => (
         <div>
         <div id={index} className="pt-4 flex justify-between">
@@ -77,7 +78,7 @@ function App() {
 
 
     <div className="max-w-[1200px] m-auto p-4">
-      <h1 className="text-2xl">Additional</h1>
+      <h1 className="text-2xl text-dblue">Additional</h1>
       <h1 className="text-lg pt-4">Technical Skills</h1>
       <h1 className="pt-2">Programming Languages: <span className="italic">{content.additional.PL}</span></h1>
       <h1 className="pt-1">Frameworks: <span className="italic ">{content.additional.libraries}</span></h1>
--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="small nav w-screen text-white fixed top-0 right-0">
         <div className="bar max-w-[1560px] m-auto flex justify-between p-4 items-center">
             <div className="flex justify-between items-center w-full pl-4">
-            <div className="font-bold text-xl">Ojas Baral</div>
+            <div className="font-bold text-xl text-dblue">Ojas Baral</div>
             {collapsed?<AiOutlineMenu size={23} className="menu visible md:invisible cursor-pointer" onClick={() => collapsed?setCollapsed(false):setCollapsed(true)}></AiOutlineMenu>:<AiOutlineClose  size={23} className="menu visible md:invisible cursor-pointer" onClick={() => collapsed?setCollapsed(false):setCollapsed(true)}></AiOutlineClose>}
             </div>
             <ul className={' elements header-drop-down-enter flex list sm:justify-between sm:items-center md:gap-4 gap-2 pt-2 text-center text-base w-full sm:w-auto'}>
